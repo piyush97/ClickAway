f161eab59322915ffe4acede9606e048acc3137d
Piyush Mehta <me@piyushmehta.com>
2021-03-08 21:31:38 +0530

    title

--- pages/home.tsx
+++ pages/home.tsx
@@ -6,6 +6,9 @@ import Hero from '../component/hero';
 export default function Home() {
   return (
     <>
+      <header>
+        <title>Click Away | Deals just a click away</title>
+      </header>
       <div style={{ backgroundColor: '#38B2AC', height: '0.4rem' }} />
       <Container maxW="container.xl">
         <Header />
