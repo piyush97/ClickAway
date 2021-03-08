1d03c8a7b0206692357745b22aa4dc3da4e1ef87
Piyush Mehta <me@piyushmehta.com>
2021-03-08 21:39:55 +0530

    fixed types

---
+++ utils/theme.ts
@@ -0,0 +1,15 @@
+import { ColorMode, extendTheme } from '@chakra-ui/react';
+// 2. Add your color mode config
+
+export interface ChakraConfig {
+  initialColorMode: ColorMode;
+  useSystemColorMode: boolean;
+}
+
+const config: ChakraConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+// 3. extend the theme
+const theme = extendTheme({ config });
+export default theme;
