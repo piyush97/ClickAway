fd31602404576d079b8a659f36cb57715efad7eb
Piyush Mehta <me@piyushmehta.com>
2021-03-09 16:42:32 +0530

    some fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,46 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: { jsx: true },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    env: {
-      browser: true,
-      node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:jsx-a11y/recommended',
-      // Prettier plugin and recommended rules
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended',
-    ],
-    rules: {
-      // Include .prettierrc.js rules
-      'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-      'react/prop-types': 'off',
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/ban-ts-ignore': 'off',
-      'jsx-a11y/label-has-associated-control': [
-        'error',
-        {
-          labelComponents: [],
-          labelAttributes: [],
-          controlComponents: [],
-          assert: 'either',
-          depth: 25,
-        },
-      ],
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-    settings: {
-      react: {
-        version: 'detect',
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier/@typescript-eslint',
+    'simple-import-sort',
+    'plugin:prettier/recommended', // Make sure this is always the last element in the array.
+  ],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'simple-import-sort/sort': 'error',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
       },
-    },
-  };
+    ],
+  },
+};
